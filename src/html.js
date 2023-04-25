80a2f7dfb04563a8ffe8a3826aae51bd47205192
irrationnelle <drakkarverenis@gmail.com>
2023-04-25 18:27:57 +0900

    feat: Add clarity

--- src/html.js
+++ src/html.js
@@ -12,6 +12,13 @@ export default class HTML extends React.Component {
             name="viewport"
             content="width=device-width, initial-scale=1, shrink-to-fit=no, minimum-scale=1, maximum-scale=1, user-scalable=0, viewport-fit=cover"
           />
+          <script type="text/javascript">
+    (function(c,l,a,r,i,t,y){
+        c[a]=c[a]||function(){(c[a].q=c[a].q||[]).push(arguments)};
+        t=l.createElement(r);t.async=1;t.src="https://www.clarity.ms/tag/"+i;
+        y=l.getElementsByTagName(r)[0];y.parentNode.insertBefore(t,y);
+    })(window, document, "clarity", "script", process.env.CLARITY_API_KEY);
+</script>
           {this.props.headComponents}
         </head>
         <body {...this.props.bodyAttributes}>
