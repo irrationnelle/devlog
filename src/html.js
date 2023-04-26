8b9c429d6206f54077c1397ec1aa2b401061f6b4
irrationnelle <drakkarverenis@gmail.com>
2023-04-26 20:54:48 +0900

    feat: Add clarity in componentDidMount

--- src/html.js
+++ src/html.js
@@ -2,6 +2,14 @@ import React from 'react'
 import PropTypes from 'prop-types'
 
 export default class HTML extends React.Component {
+    componentDidMount() {
+        (function(c,l,a,r,i,t,y){
+            c[a]=c[a]||function(){(c[a].q=c[a].q||[]).push(arguments)};
+            t=l.createElement(r);t.async=1;t.src="https://www.clarity.ms/tag/"+i;
+            y=l.getElementsByTagName(r)[0];y.parentNode.insertBefore(t,y);
+        })(window, document, "clarity", "script", process.env.CLARITY_API_KEY);
+    }
+
   render() {
     return (
       <html {...this.props.htmlAttributes}>
@@ -13,11 +21,8 @@ export default class HTML extends React.Component {
             content="width=device-width, initial-scale=1, shrink-to-fit=no, minimum-scale=1, maximum-scale=1, user-scalable=0, viewport-fit=cover"
           />
           <script type="text/javascript">
-    (function(c,l,a,r,i,t,y){
-        c[a]=c[a]||function(){(c[a].q=c[a].q||[]).push(arguments)};
-        t=l.createElement(r);t.async=1;t.src="https://www.clarity.ms/tag/"+i;
-        y=l.getElementsByTagName(r)[0];y.parentNode.insertBefore(t,y);
-    })(window, document, "clarity", "script", process.env.CLARITY_API_KEY);
+            <script>
+            </script>
 </script>
           {this.props.headComponents}
         </head>
